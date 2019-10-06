488f0eb3776e45824bc01207151e3e5fb0892c05
r-veiga <roberto.aprende.mucho@gmail.com>
2019-10-07 00:02:59 +0200

    2019.10.07 r-veiga primer commit

---
+++ src/main/java/guru/springframework/spr5didemo/Spr5DiDemoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spr5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spr5DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spr5DiDemoApplication.class, args);
+	}
+
+}
